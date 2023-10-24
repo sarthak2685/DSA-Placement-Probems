263935d165f95015e781a36206eb4fc1a5f00728
Anujjoshi021 <anujjoshi595@gmail.com>
2023-10-24 15:14:03 +0530

    adding Max _data_node

---
+++ C++/Stack/Max_data_node.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+template <typename T>
+class TreeNode {
+   public:
+    T data;
+    vector<TreeNode<T>*> children;
+
+    TreeNode(T data) { this->data = data; }
+
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+
+
+TreeNode<int>* maxDataNode(TreeNode<int>* root) {
+  TreeNode<int> *max = new TreeNode<int>(INT_MIN);
+  queue<TreeNode<int> *> pn;
+  pn.push(root);
+
+  while (pn.size() != 0) {
+    TreeNode<int> *front = pn.front();
+    pn.pop();
+    if (front->data > max->data)
+      max = front;
+
+    for (int i = 0; i < front->children.size(); i++) {
+      pn.push(front->children[i]);
+    }
+  }
+  return max;
+}
+
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int>*> pendingNodes;
+
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+int main() {
+    TreeNode<int>* root = takeInputLevelWise();
+    TreeNode<int>* ans = maxDataNode(root);
+
+    if (root != NULL) {
+        cout << ans->data;
+    }
+}
